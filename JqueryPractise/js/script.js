953fde79cf1a2c38f9a9c34e18c8e0e3b2e23a6c
zzakirr <7nez6ws@code.edu.az>
2022-05-23 10:04:05 +0400

    comiit file

---
+++ JqueryPractise/js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    
+    // $("button").click(function(){
+    //     $("p").hide();
+    // });
+
+    // $('button').click(function(){
+    //     $('#test').hide();
+    // })
+
+    // $('button').click(function(){
+    //     $('ul li:first').hide();
+    // })
+
+    // $('button').click(function(){
+    //     $('[href]').hide();
+    // });
+
+    // $('p').click(function(){
+    //     $(this).hide();
+    // })
+
+    // $('#p1').mouseenter(function(){
+    //     alert("мышка на месте")
+    // })
+    // $('#p1').mousedown(function(){
+    //     alert("мышка на месте нажата")
+    // })
+
+    // $('input').focus(function(){
+    //     $(this).css('background-color', 'yellow')
+    // })
+    // $('input').blur(function(){
+    //     $(this).css('background-color', 'green')
+    // })
+
+    // $('p').on({
+    //     mouseenter: function(){
+    //         $(this).css('background-color','red');
+    //     },
+    //     mouseleave: function(){
+    //         $(this).css('background-color','blue')
+    //     },
+    //     click: function(){
+    //         $(this).css('background-color','green')
+    //     }
+    // })
+
+    
+});
---
+++ Quarter_Shop/js/script.js
@@ -0,0 +1,71 @@
+let home = document.querySelectorAll('.menu li');
+let homeMenu = document.querySelector('.homeMenu');
+home[0].addEventListener('mouseover',function(){
+    homeMenu.style.display = "block";
+})
+home[0].addEventListener('mouseout',function(){
+    homeMenu.style.display = "none";
+})
+homeMenu.addEventListener('mouseover',function(){
+    homeMenu.style.display = "block";
+})
+homeMenu.addEventListener('mouseout',function(){
+    homeMenu.style.display = "none";
+})
+
+let aboutMenu = document.querySelector('.aboutMenu')
+home[1].addEventListener('mouseover',function(){
+    aboutMenu.style.display = "block";
+})
+home[1].addEventListener('mouseout',function(){
+    aboutMenu.style.display = "none";
+})
+aboutMenu.addEventListener('mouseover',function(){
+    aboutMenu.style.display = "block";
+})
+aboutMenu.addEventListener('mouseout',function(){
+    aboutMenu.style.display = "none";
+})
+
+let shopMenu = document.querySelector('.shopMenu');
+home[2].addEventListener('mouseover',function(){
+    shopMenu.style.display = "block";
+})
+home[2].addEventListener('mouseout',function(){
+    shopMenu.style.display = "none";
+})
+shopMenu.addEventListener('mouseover',function(){
+    shopMenu.style.display = "block";
+})
+shopMenu.addEventListener('mouseout',function(){
+    shopMenu.style.display = "none";
+})
+
+let newsMenu = document.querySelector('.newsMenu');
+home[3].addEventListener('mouseover',function(){
+    newsMenu.style.display = "block";
+})
+home[3].addEventListener('mouseout',function(){
+    newsMenu.style.display = "none";
+})
+newsMenu.addEventListener('mouseover',function(){
+    newsMenu.style.display = "block";
+})
+newsMenu.addEventListener('mouseout',function(){
+    newsMenu.style.display = "none";
+})
+
+let pagesMenu = document.querySelector('.pagesMenu');
+home[4].addEventListener('mouseover',function(){
+    pagesMenu.style.display = "block";
+})
+home[4].addEventListener('mouseout',function(){
+    pagesMenu.style.display = "none";
+})
+pagesMenu.addEventListener('mouseover',function(){
+    pagesMenu.style.display = "block";
+})
+pagesMenu.addEventListener('mouseout',function(){
+    pagesMenu.style.display = "none";
+})
+
